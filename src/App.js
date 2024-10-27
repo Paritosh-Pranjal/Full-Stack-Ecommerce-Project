7cce7a39aa27a5edfcf7538fdf47335cb236a2c1
paritoshpranjal <paritoshpranjal3@gmail.com>
2024-10-28 00:53:13 +0530

    feat: define ui for home page , carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navigation from "./customer/components/Navigation/navigation";
+import HomePage from "./customer/pages/HomePage/homePage";
 
 function App() {
   return (
     <div className="App">
-      Hello Ecommerce
+      <Navigation />
+      <div>
+        <HomePage />
+      </div>
     </div>
   );
 }
---
+++ src/customer/components/HomeCarousel/mainCarousel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { mainCarouselData } from "./mainCarouselData";
+
+const MainCarousel = () => {
+  const items = mainCarouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      onClick={() => {}}
+      src={item.image}
+      alt="carousel item"
+      role="presentation"
+    />
+  ));
+
+  return (
+    <AliceCarousel
+      mouseTracking
+      items={items}
+      controlsStrategy="alternate"
+      disableButtonsControls
+      autoPlay
+      autoPlayInterval={1000}
+      infinite
+    />
+  );
+};
+
+export default MainCarousel;
